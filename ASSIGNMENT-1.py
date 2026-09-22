0eb3b6df81a991f8bf612d8363192bc1f6320077
1272262223y-sketch <1272262223.y@mitwpu.edu.in>
2026-09-22 06:50:59 +0530

    Update ASSIGNMENT-1.py

--- ASSIGNMENT-1.py
+++ ASSIGNMENT-1.py
@@ -1,14 +1,14 @@
 students = {
-    101: ("Alice", "CSE", [85, 90, 88]),
-    102: ("Bob", "ECE", [78, 82, 80]),
-    103: ("Charlie", "ME", [92, 89, 95])
+    101: ("Ansh", "CSE", [85, 90, 88]),
+    102: ("Arham", "ECE", [78, 82, 80]),
+    103: ("Aayush", "ME", [92, 89, 95])
 }
 
-students[104] = ("Diana", "IT", [88, 79, 91])
+students[104] = ("Diya", "IT", [88, 79, 91])
 
 del students[102]
 
-students[101] = ("Alice", "CSE", [90, 92, 95])
+students[101] = ("Ansh", "CSE", [90, 92, 95])
 
 print("Final Student Records:")
 for roll_no, details in students.items():
